3ac10c3918c6034f3880813bcd7423c8f0aa29b0
Lakindu Kariyawasam <lakindu1997@gmail.com>
2021-12-24 09:03:10 +0000

    functions to add bot to voice channel

--- music.py
+++ music.py
@@ -7,5 +7,32 @@ class music(commands.Cog):
   def __init__(self, client):
     self.client = client
 
+
+# join bot to channel
+  @commands.command()
+  async def join(self,ctx):
+    
+  # check if user is in a voice channel
+    if ctx.author.voice is None:
+      await ctx.send("You are not in a voice channel!")
+    voice_channel = ctx.author.voice.channel
+    # check if bot is in a voice channel
+    if ctx.voice_client is None:
+      await voice_channel.connect()
+    else:
+      await ctx.voice_client.move_to(voice_channel)
+  
+  @commands.command()
+  async def disconnect(self,ctx):
+    await ctx.voice_client.disconnect()
+
+
+# music play command
+  @commands.command()
+  async def play(self,ctx,url):
+    ctx.voice_client.stop()
+
+
+
 def setup(client):
   client.add_cog(music(client))
