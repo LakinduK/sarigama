9d63b794ccb225398e50f6818134a767bd46a323
Lakindu Kariyawasam <lakindu1997@gmail.com>
2021-12-24 09:11:56 +0000

    audio play function

--- music.py
+++ music.py
@@ -29,8 +29,20 @@ class music(commands.Cog):
 
 # music play command
   @commands.command()
-  async def play(self,ctx,url):
+  async def p(self,ctx,url):
     ctx.voice_client.stop()
+
+    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    YDL_OPTIONS = {'format':"bestaudio"}
+    vc = ctx.voice_client
+
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+      info = ydl.extract_info(url, download=False)
+      url2 = info['formats'] [0]['url']
+
+      # create stream to play audio
+      source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+      vc.play(source)
 
 
 
